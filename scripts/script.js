3705a7d971abb6d5463e6fa48f4e87c9c9c3e9cb
Magomed Akhmaev <m.axmaev@yandex.ru>
2025-09-30 18:01:21 +0300

    Several functions have been initialized

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-let userInput = document.querySelector('main__string');
+let userInput = document.querySelector('userInput');
 let strSlice = document.querySelector('stringSlice');
 let strUpperCase = document.querySelector('stringToUpperCase');
 let strLowerCase = document.querySelector('stringToLowerCase');
@@ -6,3 +6,20 @@ let strReplace = document.querySelector('stringReplace');
 let strTrim = document.querySelector('stringTrim');
 let strSplit = document.querySelector('stringSplit');
 let strEncrypt = document.querySelector('stringEncrypt');
+
+
+const sliceString = (str, i) =>{
+   return str.slice(i);
+}
+
+const stringToUpperCase = (str) =>{
+    return str.toUpperCase();
+}
+
+const stringToLowerCase = (str)=>{
+    return str.toLowerCase();
+}
+
+const replaceString = (str) =>{
+    return str.replace();
+}
