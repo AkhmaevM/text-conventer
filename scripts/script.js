7015f559f891fb4c74c91c931740bee37d327089
AkhmaevM <m.axmaev@yandex.ru>
2025-10-02 23:59:39 +0300

    Added function trim

--- scripts/script.js
+++ scripts/script.js
@@ -9,39 +9,50 @@ let strSplit = document.querySelector('.stringSplit');
 let strEncrypt = document.querySelector('.stringEncrypt');
 let copyResult = document.querySelector('.btnCopy')
 
-const sliceString = (str, i) =>{
-   return str.slice(i);
+const sliceString = (str, i) => {
+    return str.slice(i);
 }
 
-const stringToUpperCase = (str) =>{
+const stringToUpperCase = (str) => {
     return str.toUpperCase();
 }
 
-const stringToLowerCase = (str)=>{
+const stringToLowerCase = (str) => {
     return str.toLowerCase();
 }
 
-const replaceString = (str) =>{
+const replaceString = (str) => {
     return str.replace();
 }
 
-strUpperCase.onclick = function(){
+const trimString = (str) => {
+    return str.trim();
+}
+
+strTrim.onclick = function () {
+    result.value = trimString(userInput.value);
+    userInput.value = '';
+}
+
+
+
+strUpperCase.onclick = function () {
     console.log('click')
     result.value = stringToUpperCase(userInput.value);
     userInput.value = '';
 
 }
 
-copyResult.onclick = function(){
+copyResult.onclick = function () {
     navigator.clipboard.writeText(result.value)
-    .then(() => {
-        console.log('Скопировано')
-        copyResult.textContent = 'Скопировано!';
-        
-    })
-    .catch(error => {
-        console.error(`Текст не скопирован ${error}`)
-  })
+        .then(() => {
+            console.log('Скопировано')
+            copyResult.textContent = 'Скопировано!';
 
-  result.value = '';
+        })
+        .catch(error => {
+            console.error(`Текст не скопирован ${error}`)
+        })
+
+    result.value = '';
 }
